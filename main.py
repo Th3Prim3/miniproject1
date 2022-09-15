e76bbcef2d5195ecc5830551bbd0e4ba139d72d8
Jason Zeller <jlzeller@fhsu.edu>
2022-09-15 09:57:57 -0500

    Added MSFT as an export PNG file.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@
 
 
 import yfinance as yf
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 data = yf.download("MSFT", start="2022-08-30", end="2022-09-14")
 
@@ -13,3 +16,14 @@ for price in data['Adj Close']:
     msftPrices.append(price)
 
 print(msftPrices)
+
+
+# Create a NumPy array
+msftarray = np.array(msftPrices)
+
+# Create matplotlib graph
+plt.plot(msftarray)
+
+# Show the graph
+
+plt.savefig('charts/msft.png')
